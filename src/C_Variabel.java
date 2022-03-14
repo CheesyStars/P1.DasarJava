986cd59138a92b7dcf00f6399c68c553a23f7479
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-03-14 15:42:13 +0700

    Percobaan 3 Penamaan Variabel (Identifier)

---
+++ src/C_Variabel.java
@@ -0,0 +1,8 @@
+public class C_Variabel {
+    public static void main(String[] args) {
+        //Memberikan Nama Atau Mendeklaraksikan Variabel
+        double salary;
+        long _distance;
+        boolean $flag;
+    }
+}
