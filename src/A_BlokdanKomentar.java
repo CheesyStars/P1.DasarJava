1871d6822e8689f6cff52201157c3a36411d4804
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-03-14 14:59:45 +0700

    Initial Project Pertemuan 1 Dasar Java

---
+++ src/A_BlokdanKomentar.java
@@ -0,0 +1,15 @@
+public class A_BlokdanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek Dengan Java");
+
+        //Contoh komentar 1 Baris
+
+        /*
+        Contoh Komentar Lebih Dari Satu Bairs
+         */
+
+        /**
+         * Java Doc --> Baris Ini Untuk Mendokumentasikan Kode Program
+         */
+    }
+}
